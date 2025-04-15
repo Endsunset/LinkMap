c8488d6b8d8e34e6616f1c0cb69a97df30d88b94
EndSunset <hutsunemiku_redstone@icloud.com>
2025-04-15 17:20:52 +0800

    Initial Commit

---
+++ LinkMap/LinkMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkMapApp.swift
+//  LinkMap
+//
+//  Created by Yuan Ping Ke on 2025/4/15.
+//
+
+import SwiftUI
+
+@main
+struct LinkMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
