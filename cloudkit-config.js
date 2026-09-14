76e701e0adf8d34585c3fe33c60574bb7174e8fe
Endxi <hutsunemiku_redstone@icloud.com>
2026-09-14 21:54:00 +0800

    Configure production CloudKit web API token

--- cloudkit-config.js
+++ cloudkit-config.js
@@ -2,5 +2,5 @@
 window.LINKMAP_CLOUDKIT = Object.freeze({
   containerIdentifier: "iCloud.name.Endsunset.LinkMap",
   environment: "production",
-  apiToken: ""
+  apiToken: "003fd98269183dd6b6ed2692bb3edea3d2fbf9d8af4751fffb7a52ed4f056aa3"
 });
